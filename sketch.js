4f1246c6f30d5dbae3d32eea760da032f0e0de0e
IbrahimM-2012 <69331230+IbrahimM-2012@users.noreply.github.com>
2020-08-11 21:00:58 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var fixed,moving;
+
+function setup() {
+  createCanvas(800,400);
+fixed = createSprite(200,200,50,80);
+moving = createSprite(400,200,80,50);
+fixed.shapeColor="blue";
+moving.shapeColor="green";
+fixed.debug=true;
+moving.debug=true;
+}
+
+function draw() {
+  background(255,255,255);  
+  moving.x=World.mouseX;
+  moving.y=World.mouseY;
+  if(moving.x-fixed.x<fixed.width/2+moving.width/2 &&fixed.x-moving.x<fixed.width/2+moving.width/2 &&moving.y-fixed.y<moving.height/2+fixed.height/2 &&fixed.y-moving.y<fixed.height/2+moving.height/2){
+    moving.shapeColor="red";
+    fixed.shapeColor="darkred";
+  }
+  else {
+    moving.shapeColor="lightyellow";
+    fixed.shapeColor="darkyellow";
+  }
+  drawSprites();
+
+}
